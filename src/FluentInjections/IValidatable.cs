84f3e430d8b9432b0460f5b278a239ff970ce08e
Ulf Bourelius <159780773+ulfbou@users.noreply.github.com>
2024-12-19 13:17:47 +0100

    Added XML commented documentation

--- src/FluentInjections/IValidatable.cs
+++ src/FluentInjections/IValidatable.cs
@@ -1,6 +1,13 @@
 ﻿namespace FluentInjections;
 
+/// <summary>
+/// Represents an interface for objects that can be validated.
+/// </summary>
 public interface IValidatable
 {
+    /// <summary>
+    /// Validates the current state of the object.
+    /// </summary>
+    /// <exception cref="ValidationException">Thrown when the object is in an invalid state.</exception>
     void Validate();
 }
