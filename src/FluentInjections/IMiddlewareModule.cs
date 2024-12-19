a73f649b70474e184253d78f7052541440a944f2
Ulf Bourelius <159780773+ulfbou@users.noreply.github.com>
2024-12-19 12:48:53 +0100

    Updated documentation

--- src/FluentInjections/IMiddlewareModule.cs
+++ src/FluentInjections/IMiddlewareModule.cs
@@ -2,11 +2,15 @@
 
 /// <summary>
 /// Represents a module that can be registered with a middleware configurator.
+/// This interface should be implemented by classes that define middleware registrations and configurations.
 /// </summary>
+/// <typeparam name="TBuilder">The type of the application builder.</typeparam>
 public interface IMiddlewareModule<TBuilder> where TBuilder : class
 {
     /// <summary>
-    /// Configures the middleware with a <see cref="IMiddlewareConfigurator{TBuilder}"/>.
+    /// Configures the middleware for this module using the provided <see cref="IMiddlewareConfigurator{TBuilder}"/>.
+    /// This method is called to register and configure middleware components within the application.
     /// </summary>
+    /// <param name="configurator">The middleware configurator used to register and configure middleware components.</param>
     void ConfigureMiddleware(IMiddlewareConfigurator<TBuilder> configurator);
 }
