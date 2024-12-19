28f6f441d4c1f085e9e6f4cbd93c2c69befc5e08
Ulf Bourelius <159780773+ulfbou@users.noreply.github.com>
2024-12-19 13:21:51 +0100

    Added XML commented documentation

--- src/FluentInjections/IInitializable.cs
+++ src/FluentInjections/IInitializable.cs
@@ -1,6 +1,13 @@
 ﻿namespace FluentInjections;
 
+/// <summary>
+/// Represents an interface for objects that require initialization.
+/// </summary>
 public interface IInitializable
 {
+    /// <summary>
+    /// Initializes the current instance.
+    /// This method should be called to perform any setup or initialization tasks required by the instance.
+    /// </summary>
     void Initialize();
 }
