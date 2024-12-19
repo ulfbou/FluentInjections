4726a7f931245d6ec16bb468f55db13a627179f0
Ulf Bourelius <159780773+ulfbou@users.noreply.github.com>
2024-12-19 12:42:01 +0100

    Update IServiceModule.cs

--- src/FluentInjections/IServiceModule.cs
+++ src/FluentInjections/IServiceModule.cs
@@ -1,12 +1,15 @@
 ﻿namespace FluentInjections;
 
 /// <summary>
-/// Represents a module that can be registered with a <see cref="IServiceConfigurator"/>.
+/// Represents a module that can be registered with an <see cref="IServiceConfigurator"/>.
+/// This interface should be implemented by classes that define service registrations and configurations.
 /// </summary>
 public interface IServiceModule
 {
     /// <summary>
-    /// Configures the service module with a <see cref="IServiceConfigurator"/>.
+    /// Configures the services for this module using the provided <see cref="IServiceConfigurator"/>.
+    /// This method is called to register and configure services within the dependency injection container.
     /// </summary>
+    /// <param name="configurator">The service configurator used to register and configure services.</param>
     void ConfigureServices(IServiceConfigurator configurator);
 }
