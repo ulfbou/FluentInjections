ca507fe65202dfac31e3d41c8649c68915bf6978
Ulf Bourelius <159780773+ulfbou@users.noreply.github.com>
2025-01-20 17:16:33 +0100

    Initial commit

---
+++ src/FluentInjections/Internal/Extensions/SemaphoreSlimExtensions.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) FluentInjections Project. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+namespace FluentInjections.Internal.Extensions;
+
+using FluentInjections.Internal.Utils;
+
+public static class SemaphoreSlimExtensions
+{
+    public static DisposableWait DisposableWait(this SemaphoreSlim semaphoreSlim)
+    {
+        return new DisposableWait(semaphoreSlim);
+    }
+}
---
+++ src/FluentInjections/Internal/Utils/DisposableWait.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) FluentInjections Project. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+namespace FluentInjections.Internal.Utils;
+
+public class DisposableWait : IDisposable
+{
+    private readonly SemaphoreSlim _semaphore;
+    public SemaphoreSlim Semaphore => _semaphore;
+
+    public DisposableWait(SemaphoreSlim semaphore)
+    {
+        _semaphore = semaphore;
+        _semaphore.Wait();
+    }
+
+    public void Dispose()
+    {
+        _semaphore.Release();
+    }
+}
