09ed75d1c504675546955c698b7575e8371d5e51
Ulf Bourelius <159780773+ulfbou@users.noreply.github.com>
2025-01-20 08:45:44 +0100

    Initial commit

---
+++ tests/FluentInjections.Tests/Units/Configurator/NetCoreEndpointConfiguratorTests.cs
@@ -0,0 +1,25 @@
+﻿using FluentInjections.Tests.Internal.Configurators;
+
+namespace FluentInjections.Tests.Units.Configurator;
+
+public class NetCoreEndpointConfiguratorTests
+{
+    private readonly InternalEndpointConfiguratorTests _internal = new InternalEndpointConfiguratorTests();
+
+    public NetCoreEndpointConfiguratorTests()
+    {
+        _internal.Configurator = new TestNetCoreEndpointConfigurator(_internal.Fixture.App, _internal.Fixture.LoggerMock.Object);
+    }
+
+    [Fact]
+    public void Map_ShouldRegisterEndpointWithCorrectPatternAndMethod()
+    {
+        _internal.Map_ShouldRegisterEndpointWithCorrectPatternAndMethod();
+    }
+
+    [Fact]
+    public void Map_ShouldRegisterEndpointWithCorrectHandler()
+    {
+        _internal.SetConflictResolutionMode_ShouldThrowExceptionForInvalidMode();
+    }
+}
